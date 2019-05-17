f6a04642ffe047c51201feedb1edaaf948e6cbd6
Matt <e123matt@gmail.com>
2019-05-16 22:10:41 -0500

    Added Misc module with Echo command

---
+++ DiscordBot/Modules/Misc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echoed Message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false, embed.Build(), null);
+        }
+    }
+}
