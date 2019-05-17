a67e95493b84a9cb44f5529712ac98eee7e4d28c
Matt <e123matt@gmail.com>
2019-05-16 21:02:47 -0500

    Initial upload of DiscordBot, bot can go online

---
+++ DiscordBot/CommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DiscordBot
+{
+    class CommandHandler
+    {
+        DiscordSocketClient _client;
+        CommandService _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            await _service.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+            var context = new SocketCommandContext(_client, msg);
+            int argPos = 0;
+            if (msg.HasStringPrefix(Config.bot.cmdPrefix, ref argPos)
+                || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos, null, MultiMatchHandling.Best);
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
